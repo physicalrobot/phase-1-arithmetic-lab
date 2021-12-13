4095ca1cd354a26e55421d2b42b8d0cea4f0f38a
Vikalp Malhotra <kimboho@gmail.com>
2021-12-13 15:01:34 -0500

    commit tree 845d6565f4c7210c434408175e0b2374f157f4a6

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() + 1)
+
+let num3 = 9;
+let num4 = 5;
+
+let mod = num3 % num4
+
+let max = Math.max(1, 3, 4, 5, 6, 20);
